09dc3e6247f9092cc61d66c0788956083cab3858
Ivo Iliev <133795870+elsifeither@users.noreply.github.com>
2023-08-13 18:00:58 +0300

    Add files via upload
    practise

---
+++ Practise/DecimalToBinaryConverter03practise.java
@@ -0,0 +1,28 @@
+package StacksAndQueuesPractise;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinaryConverter03practise {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number = Integer.parseInt(sc.nextLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        if (number == 0) {
+            System.out.println(0);
+        }
+
+        while (number != 0) {
+            stack.push(number % 2);
+            number /= 2;
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+
+    }
+}
