25de163359949fea212984c00d955bec6db3b74f
Ivo Iliev <133795870+elsifeither@users.noreply.github.com>
2023-10-31 17:22:21 +0200

    Add files via upload

---
+++ WorkingWithAbstraction/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle {
+    private Point bottomLeft;
+    private Point topRight;
+
+    public Rectangle(Point bottomLeft, Point topRight) {
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+    }
+
+
+    public boolean Contains(Point point) {
+        return point.getCoordinatesX() >= bottomLeft.getCoordinatesX() && point.getCoordinatesX() <= topRight.getCoordinatesX()
+                && point.getCoordinatesY() >= bottomLeft.getCoordinatesY() && point.getCoordinatesY() <= topRight.getCoordinatesY();
+    }
+}
